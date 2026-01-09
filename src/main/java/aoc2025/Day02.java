cd7541d8bf0c12fe6cb4f16068f4af9eaec52aa2
LeoB <leo.baric8@gmail.com>
2026-01-09 23:17:17 +0100

    day 2 solution

--- src/main/java/aoc2025/Day02.java
+++ src/main/java/aoc2025/Day02.java
@@ -1,6 +1,7 @@
 package aoc2025;
 
 import java.io.IOException;
+import java.util.Arrays;
 
 public class Day02 extends Day {
 
@@ -8,13 +9,84 @@ public class Day02 extends Day {
         super(2);
     }
 
+    // Parse comma-separated ranges of product IDs. Find all IDs within ranges
+    // that consist of a digit sequence repeated exactly twice (e.g., 11, 6464, 123123).
+    // Sum all such invalid IDs.
     public String part1() throws IOException {
         String inputString = super.readInput();
+        String[] rows = inputString.split(",");
+        long invalidIdSum = 0;
 
-        return "2.1";
+        for (String row: rows) {
+            String[] edges = row.split("-");
+            long startingEdge = Long.parseLong(edges[0].trim());
+            long endingEdge = Long.parseLong(edges[1].trim());
+
+            for (long i = startingEdge; i <= endingEdge; i++) {
+                if(!isValid(i)) {
+                    invalidIdSum += i;
+                }
+            }
+        }
+
+        return Long.toString(invalidIdSum);
     }
 
-    public String part2() {
-        return "2.2";
+
+    public boolean isValid(long number) {
+        final String numberString = Long.toString(number);
+
+        // early return if number length is not even
+        if(numberString.length() % 2 != 0) {
+            return true;
+        }
+
+        String firstHalf = numberString.substring(0, numberString.length()/2);
+        String secondHalf = numberString.substring(numberString.length()/2);
+
+        return !firstHalf.equals(secondHalf);
+    }
+
+    // Same as part 1, but invalid IDs are now digit sequences repeated
+    // two or more times (e.g., 11, 123123, 1234123412341234). Sum all invalid IDs.
+    public String part2() throws IOException {
+        String inputString = super.readInput();
+        String[] rows = inputString.split(",");
+        long invalidIdSum = 0;
+
+        for (String row: rows) {
+            String[] edges = row.split("-");
+            long startingEdge = Long.parseLong(edges[0].trim());
+            long endingEdge = Long.parseLong(edges[1].trim());
+
+            for (long i = startingEdge; i <= endingEdge; i++) {
+                if(!isValid2(i)) {
+                    invalidIdSum += i;
+                }
+            }
+        }
+
+        return Long.toString(invalidIdSum);
+    }
+
+    public boolean isValid2(long number) {
+        final String numberString = Long.toString(number);
+        final int numberStringLength = numberString.length();
+
+        for(int i = 1; i <= numberStringLength/2; i++) {
+            String tempSubstring = numberString.substring(0, i);
+
+            if(numberStringLength % tempSubstring.length() != 0) {
+                continue;
+            }
+
+            final String regex = "(?<=\\G.{" + tempSubstring.length() + "})";
+            String[] substrings = numberString.split(regex);
+
+            boolean allSame = Arrays.stream(substrings).allMatch(tempSubstring::equals);
+            if(allSame) return false;
+        }
+
+        return true;
     }
 }
